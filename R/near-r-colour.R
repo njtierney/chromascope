9892f6e38f98b23e1a5f121465fb5fe7731b80ff
Zhuan Leong, Vis <zhuan.leong@monash.edu>
2025-11-19 16:34:13 +1100

    formatting

--- R/near-r-colour.R
+++ R/near-r-colour.R
@@ -3,15 +3,18 @@ nearRcolor <- function(
   cSpace = c("hsv", "rgb255", "Luv", "Lab"),
   dist = switch(cSpace, "hsv" = 0.10, "rgb255" = 30, "Luv" = 15, "Lab" = 12)
 ) {
-  if (is.character(rgb)) rgb <- col2rgb(rgb)
+  if (is.character(rgb)) {
+    rgb <- col2rgb(rgb)
+  }
   stopifnot(length(rgb <- as.vector(rgb)) == 3)
   Rcol <- col2rgb(.cc <- colors())
   uniqC <- !duplicated(t(Rcol)) # gray9 == grey9 (etc)
   Rcol <- Rcol[, uniqC]
   .cc <- .cc[uniqC]
   cSpace <- match.arg(cSpace)
-  convRGB2 <- function(Rgb, to)
+  convRGB2 <- function(Rgb, to) {
     t(convertColor(t(Rgb), from = "sRGB", to = to, scale.in = 255))
+  }
   ## the transformation,  rgb{0..255} --> cSpace :
   TransF <- switch(
     cSpace,
