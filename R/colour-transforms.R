a6468d008a4aa8c9991c7f7ba2dc0722ff7af188
njtierney <nicholas.tierney@gmail.com>
2025-11-19 16:04:05 +1100

    simplify functions by using farver

--- R/colour-transforms.R
+++ R/colour-transforms.R
@@ -1,11 +1,70 @@
+#' Convert colour to hex value
+#'
+#' @description go from a colour like "red" to the hex code, "#FF0000"
+#'
+#' @param colour a colour name from [colours()] or [colors()]
+#'
+#' @returns
+#' A hex color hex value
+#'
+#' @export
+#' @examples
+#' col2hex("red")
+col2hex <- function(colour) {
+  farver::encode_native(colour) |>
+    farver::decode_native()
+}
+
+#' Convert colour to HCL values
+#'
+#' @description go from a colour like "red" to a HCL matrix, values of hue,
+#'   chroma, and luminance
+#'
+#' @inheritParams col2hex
+#'
+#' @returns A matrix of hue, chroma, and luminance
+#'
+#' @export
+#' @examples
+#' col2hcl("red")
+col2hcl <- function(colour) {
+  farver::decode_colour(colour, to = "hcl")
+}
+
+#' Convert colour to RGB values
+#'
+#' @description go from a colour like "red" to a HCL matrix, values of red,
+#' green, and blue.
+#'
+#' @inheritParams col2hex
+#'
+#' @returns A matrix of red, green, and blue.
+#'
+#' @export
+#' @examples
+#' col2rgb("red")
+col2rgb <- function(colour) {
+  farver::decode_colour(colour, to = "rgb")
+}
+
+#' Create a data frame of colour values
+#'
+#' @inheritParams col2hex
+#'
+#' @returns data frame of colours with columns: name, hex, hue, chroma,
+#'   luminance, red, green, blue
+#' @export
+#'
+#' @examples
+#' colour_data("red")
 colour_data <- function(colour) {
-  tibble(
+  tibble::tibble(
     name = colour,
     hex = col2hex(colour)
   ) |>
-    bind_cols(col2hcl(colour)) |>
-    bind_cols(col2rgb(colour)) |>
-    rename(
+    dplyr::bind_cols(col2hcl(colour)) |>
+    dplyr::bind_cols(col2rgb(colour)) |>
+    dplyr::rename(
       hue = h,
       chroma = c,
       luminance = l,
@@ -13,16 +72,4 @@ colour_data <- function(colour) {
       green = g,
       blue = b
     )
-}
-
-col2hex <- function(colour) {
-  encode_native(colour) |> decode_native()
-}
-
-col2hcl <- function(colour) {
-  decode_colour(colour, to = "hcl")
-}
-
-col2rgb <- function(colour) {
-  decode_colour(colour, to = "rgb")
 }
